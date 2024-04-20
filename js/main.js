7c6a09ceff27e85999feb3b1854d3997d2d37207
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-04-20 18:44:41 -0300

    Add iniciarCompra() with primitive functionality
    When confirmed it fires a sweetAlert dialog and toast

--- js/main.js
+++ js/main.js
@@ -240,6 +240,44 @@ function calcularTotal()
     TOTAL.textContent = precio_total;
 };
 
+const COMPRAR_BUTTON = document.getElementById("comprarCarrito");
+
+COMPRAR_BUTTON.addEventListener("click", () => iniciarCompra());
+
+function iniciarCompra()
+{
+    const TOTAL = document.getElementById("totalCarrito").children[1];
+
+    Swal.fire({
+        title: '¿Continuar con la compra?',
+        text: `El costo total es de $${parseFloat(TOTAL.textContent)}`,
+        icon: 'question',
+        showDenyButton: 'true',
+        denyButtonText: 'No, continuar comprando',
+        confirmButtonText: 'Si, terminar compra'
+    }).then((respuesta) => {
+        if (respuesta.isConfirmed)
+        {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            
+            Toast.fire({
+                icon: "success",
+                title: "¡Compra realizada con éxito!"
+            });
+        }
+    });
+};
+
 function simuladorHavannaGUI()
 {
     cargarListadoProductos();
